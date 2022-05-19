a248e8b433d7be68044d4bb8160de92f99b52d82
Will Verland <will.verland@xero.com>
2022-05-20 08:06:33 +1200

    Add API with entity DB

---
+++ DatabaseStreamingAPI/Person.cs
@@ -0,0 +1,13 @@
+﻿namespace DatabaseStreamingAPI
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string AvatarUrl { get; set; }
+        public DateTime Birthday { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public string Notes { get; set; }
+    }
+}
---
+++ DatabaseStreamingAPI/PersonContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DatabaseStreamingAPI
+{
+    public class PersonContext : DbContext
+    {
+        public DbSet<Person> People => Set<Person>();
+
+        public PersonContext(DbContextOptions<PersonContext> options)
+            : base(options)
+        {
+        }
+    }
+}
